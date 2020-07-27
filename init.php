5c59cc162a41525d81b5d6cc1d27fe6797bb2666
Durk Steed <durksteed13@gmail.com>
2020-07-27 16:48:48 -0400

    DB connection and init file

---
+++ init.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+$GLOBALS['config'] = array(
+	'mysql' => array(
+		'host' =>'ruby.db.elephantsql.com',
+		'username' => 'qgsmikpz',
+		'password' => 'yLWbKfzlvzK0BrVq9vBCcpRjx21fEBFs',
+		'db' => 'KeepMyThoughts'
+	),
+	'remember' => array(
+		'cookie_name' => 'hash',
+		'cookie_expiry' => 604800
+	),
+	'session' => array(
+		'session_name' => 'user')
+);
+
+spl_autoload_register(function($class) {
+	require_once 'classes/' . $class . '.php';
+});
+
+require_once 'functions/scrubber.php'
