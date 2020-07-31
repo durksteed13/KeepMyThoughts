afff1cdf8efa2e385dcd5a7d9521f8d041ab454b
Durk Steed <durksteed13@gmail.com>
2020-07-31 14:38:02 -0400

    signup/login forms styled, mockup image section complete on landing

--- login.php
+++ login.php
@@ -36,28 +36,7 @@ if(Input::exists()) {
 </head>
 <?php include 'html/header.html';?>
 <body>
-	<!-- <?php include 'html/landing.html';?> -->
-	<!-- <?php include 'html/footer.html';?> -->
+<?php include 'html/login.html';?>
 </body>
-
-<form action="" method="post">
-	<div class="field">
-		<label for="username">Username</label>
-		<input type="text" name="username" id="username" autocomplete="off">
-	</div>
-
-	<div class="field">
-		<label for="password">Password</label>
-		<input type="password" name="password" id="password" autocomplete="off">
-	</div>
-
-	<div class="field">
-		<label for="remember">
-			<input type="checkbox" name="remember" id="remember">Remember me
-		</label>
-	</div>
-
-	<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-	<input type="submit" value="Log in">
-</form>
+<?php include 'html/footerFixed.html';?>
 </html>
--- register.php
+++ register.php
@@ -33,32 +33,7 @@ if(Input::exists()) {
 </head>
 <?php include 'html/header.html';?>
 <body>
-	<!-- <?php include 'html/landing.html';?> -->
-	<!-- <?php include 'html/footer.html';?> -->
+<?php include 'html/register.html';?>
 </body>
-
-<form action="" method="post">
-	<div class="field">
-		<label for="username">Username</label>
-		<input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')); ?>" autocomplete="off">
-	</div>
-
-	<div class ="field">
-		<label for="password">Choose a password</label>
-		<input type="password" name="password" id="password">
-	</div>
-
-	<div class ="field">
-		<label for="password_again">Enter your password again</label>
-		<input type="password" name="password_again" id="password_again">
-	</div>
-
-	<div class ="field">
-		<label for="name">Enter your name</label>
-		<input type="text" name="name" value="<?php echo escape(Input::get('name')); ?>" id="name">
-	</div>
-
-	<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-	<input type="submit" value="Register">
-</form>
+<?php include 'html/footerFixed.html';?>
 </html>
