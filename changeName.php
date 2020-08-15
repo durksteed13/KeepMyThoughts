8c82438cf0519534445ec699ebbe90a0874c3261
Durk Steed <durksteed13@gmail.com>
2020-08-15 15:00:51 -0400

    working on user page

---
+++ changeName.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'init.php';
+
+$user = new User();
+
+if(!$user->isLoggedIn()) {
+	Redirect::to('index.php');
+}
+
+if(Input::exists()) {
+	if(Token::check(Input::get('token'))) {
+		$validate = new Validate();
+		$validation = $validate->Check($_POST, array('name' => array('required' => true, 'min' => 2)));
+
+		if($validation->passed()) {
+			$user->update(array('name' => Input::get('name')));
+
+			// Session::flash('home', 'Your name has been changed');
+			Redirect::to('index.php');
+		} else {
+			$errorText = '';
+			$x = 1;
+			foreach($validation->errors() as $error) {
+				$errorText .= $error . ', ';  
+			}
+			$errorText = substr($errorText, 0, -2);
+				echo "<div class='error'>{$errorText}</div>";
+		}
+	}
+}
+
+include 'changeName.html';
---
+++ upcoming.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'init.php';
+
+include 'html/upcoming.html';
